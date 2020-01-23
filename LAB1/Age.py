def8ca10317f8e923fb397e5ad5e17a2e10db3d5
vishalrk1 <59719046+vishalrk1@users.noreply.github.com>
2020-01-23 08:24:23 +0530

    Add files via upload

---
+++ LAB1/Age.py
@@ -0,0 +1,8 @@
+print("Enter your name and your year of birth : ")
+name = (input("Name: "))
+print("""Hey welcome !!!
+   Please enter your year of birth""")
+birth_year = int(input("Date of birth: "))
+age = int(birth_year) + 100
+print(name + " your age after 100 years is :")
+print(age)
---
+++ LAB1/Number.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("Entered number is even")
+else:
+   print("Entered number is odd")
